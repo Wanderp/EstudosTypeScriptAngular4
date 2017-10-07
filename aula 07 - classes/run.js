b2947396d20f1a55ab8420776640054692da59e1
Wander Paulo <Wander Paulo>
2017-10-07 00:47:37 -0300

    Projeto de testes - Typescript - types, tasks e classes

---
+++ aula 07 - classes/run.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Animal_1 = require("./Animal");
+var Cavalo_1 = require("./Cavalo");
+var a = new Animal_1.Animal("Bob");
+var c = new Cavalo_1.Cavalo("Pegasus");
+a.mover(10);
+c.mover(50);
+//# sourceMappingURL=run.js.map
---
+++ aula 07 - classes/run.ts
@@ -0,0 +1,8 @@
+import {Animal} from './Animal';
+import {Cavalo} from './Cavalo';
+
+let a = new Animal("Bob");
+let c = new Cavalo("Pegasus");
+
+a.mover(10);
+c.mover(50);
